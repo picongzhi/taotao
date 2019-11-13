4be8ef327d4fa4b01d488d4a974671660409ce52
picongzhi <picongzhi@gmail.com>
2019-11-14 01:23:53 +0800

    feat(redis): add jedis cluster test

--- taotao-content/taotao-content-service/src/test/java/com/pcz/taotao/jedis/JedisTest.java
+++ taotao-content/taotao-content-service/src/test/java/com/pcz/taotao/jedis/JedisTest.java
@@ -1,8 +1,14 @@
 package com.pcz.taotao.jedis;
 
 import org.junit.Test;
+import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
 import redis.clients.jedis.JedisPool;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class JedisTest {
     @Test
@@ -20,5 +26,22 @@ public class JedisTest {
         System.out.println(jedis.get("key"));
         jedis.close();
         jedisPool.close();
+    }
+
+    @Test
+    public void jedisClusterTest() throws IOException {
+        Set<HostAndPort> hostAndPortSet = new HashSet<>();
+        hostAndPortSet.add(new HostAndPort("127.0.0.1", 7000));
+        hostAndPortSet.add(new HostAndPort("127.0.0.1", 7001));
+        hostAndPortSet.add(new HostAndPort("127.0.0.1", 7002));
+        hostAndPortSet.add(new HostAndPort("127.0.0.1", 7003));
+        hostAndPortSet.add(new HostAndPort("127.0.0.1", 7004));
+        hostAndPortSet.add(new HostAndPort("127.0.0.1", 7005));
+        hostAndPortSet.add(new HostAndPort("127.0.0.1", 7006));
+
+        JedisCluster jedisCluster = new JedisCluster(hostAndPortSet);
+        jedisCluster.set("key", "1234567");
+        System.out.println(jedisCluster.get("key"));
+        jedisCluster.close();
     }
 }
