8093aad280b07186f9e36a6186286a2b5c5b238d
picongzhi <picongzhi@gmail.com>
2019-11-16 18:23:49 +0800

    feat(search): add item search dao

--- taotao-search/taotao-search-service/src/main/java/com/pcz/taotao/search/mapper/SearchItemMapper.java
+++ taotao-search/taotao-search-service/src/main/java/com/pcz/taotao/search/mapper/SearchItemMapper.java
@@ -1,8 +1,17 @@
 package com.pcz.taotao.search.mapper;
 
+import com.pcz.taotao.common.pojo.SearchItem;
+
+import java.util.List;
+
 /**
  * @author picongzhi
  */
 public interface SearchItemMapper {
-
+    /**
+     * 获取搜索商品
+     *
+     * @return List<SearchItem>
+     */
+    List<SearchItem> getItemList();
 }
